6acd3d3827d8116362b91e00c7af71683abc4d74
wooobo <irooori89@gmail.com>
2023-05-10 10:37:11 +0900

    feat : Amount, PositiveNumber 도메인 추가
    - Amount : 가격에 대한 책임
    - PositiveNumber : 양수 원시값 포장, 양수 관리에 대한 책임

---
+++ src/main/java/lotto/Amount.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import java.util.Objects;
+
+public class Amount {
+
+    private final PositiveNumber value;
+
+    private Amount(PositiveNumber value) {
+        validRange(value);
+
+        this.value = value;
+    }
+
+    public static Amount of(int value) {
+        return new Amount(new PositiveNumber(value));
+    }
+
+    private void validRange(PositiveNumber value) {
+        PositiveNumber MIN = new PositiveNumber(1000);
+
+        if (MIN.isLessThan(value)) {
+            throw new IllegalArgumentException(
+                String.format("최저가격 보다 커야합니다. 최저가격: %d, 시도가격: %d", MIN, value));
+        }
+    }
+
+    public PositiveNumber divide(PositiveNumber other) {
+        return this.value.divide(other);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Amount amount = (Amount) o;
+        return Objects.equals(value, amount.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/main/java/lotto/PositiveNumber.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import java.util.Objects;
+
+public class PositiveNumber {
+
+    private final int value;
+
+    public PositiveNumber(int value) {
+        validRange(value);
+
+        this.value = value;
+    }
+
+    public boolean isLessThan(PositiveNumber other) {
+        return this.value > other.value;
+    }
+
+    private void validRange(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException(String.format("음수를 가질수 없습니다. 값: %d", value));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PositiveNumber that = (PositiveNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    public PositiveNumber divide(PositiveNumber other) {
+        return new PositiveNumber(value / other.value);
+    }
+}

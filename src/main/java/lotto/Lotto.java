0fd122724fc447e0b762082dd5edfbdd64c5b041
wooobo <irooori89@gmail.com>
2023-05-10 11:06:45 +0900

    feat : Lotto, LottoNumber 도메인 추가

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,63 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    private static final int BUNDLE_COUNT = 6;
+
+    private final Set<LottoNumber> values;
+
+    private Lotto(Set<LottoNumber> values) {
+        validateLottoBundle(values);
+
+        this.values = values;
+    }
+
+    private Lotto(Set<LottoNumber> values, int originValuesSize) {
+        this(values);
+
+        validateNonDuplicate(values, originValuesSize);
+    }
+
+    public static Lotto of(int[] numbers) {
+        Set<LottoNumber> values = Arrays.stream(numbers)
+            .mapToObj(LottoNumber::of)
+            .collect(Collectors.toSet());
+
+        return new Lotto(values, numbers.length);
+    }
+
+    private void validateNonDuplicate(Set<LottoNumber> values, int validSize) {
+        if (values.size() != validSize) {
+            throw new IllegalArgumentException("중복 숫자를 입력 할 수 없습니다.");
+        }
+    }
+
+    private void validateLottoBundle(Set<LottoNumber> values) {
+        if (values.size() != BUNDLE_COUNT) {
+            throw new IllegalArgumentException(
+                String.format("로또 묶음은 %d개 입니다. 현재값 : %d", BUNDLE_COUNT, values.size()));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(values, lotto.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
+    }
+}
---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private final int value;
+
+    public LottoNumber(int value) {
+        validRange(value);
+
+        this.value = value;
+    }
+
+    public static LottoNumber of(int value) {
+        return new LottoNumber(value);
+    }
+
+    public boolean same(LottoNumber other) {
+        return this.equals(other);
+    }
+
+    private void validRange(int value) {
+        int MIN = 1;
+        int MAX = 45;
+
+        if (value < MIN || value > MAX) {
+            throw new IllegalArgumentException(
+                String.format("로또 범위에서 벗어났습니다. %d 부터 %d까지", MIN, MAX));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
